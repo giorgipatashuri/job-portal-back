40f70f5ccd78a5e8499dde34560b8ec8df1c1e00
gpatasha <patashuri2003@gmail.com>
2025-01-16 11:13:48 +0400

    added skill entity and dto

---
+++ src/main/java/by/giorgi/jobportalback/model/entity/Skill.java
@@ -0,0 +1,29 @@
+package by.giorgi.jobportalback.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "skills")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @ManyToOne
+    @JoinColumn(name = "cv_id")
+    private Cv cv;
+}
