39670508446c3ccf83c489e9e57bceba18afb35a
gpatasha <patashuri2003@gmail.com>
2025-01-22 19:32:56 +0400

    updated user service

--- src/main/java/by/giorgi/jobportalback/service/UserService.java
+++ src/main/java/by/giorgi/jobportalback/service/UserService.java
@@ -1,6 +1,7 @@
 package by.giorgi.jobportalback.service;
 
 
+import by.giorgi.jobportalback.model.entity.User;
 import by.giorgi.jobportalback.repository.UserRepository;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,14 @@ public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User getUserByUsername(String username) throws UsernameNotFoundException{
+        return userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException(username));
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
